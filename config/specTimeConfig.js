9fbf203dd13aebe7f3305063bfb1fa6f2de004cf
viinyxu <viinyxu@tencent.com>
2018-06-10 10:39:55 +0800

    change

--- config/specTimeConfig.js
+++ config/specTimeConfig.js
@@ -1,5 +1,3 @@
 module.exports = {
-	CHAMPION_RES_TIME : new Date('2018-06-15 00:00:00').getTime(),
-	GOURP_START_TIME : new Date('2018-06-01 21:00:00').getTime(),
-	GROUP_DEADLINR : new Date('2018-06-14 00:00:00').getTime()
+	GOURP_START_TIME : 1527858000000,
 }
